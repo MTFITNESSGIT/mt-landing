4538aa72f1c43dbe844bed68008f1fda09438410
Juan Segundo <juansepincha@gmail.com>
2025-06-08 23:29:54 -0300

    feat: :art: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Principal from "@/components/Principal";
 import Whatsapp from "@/components/Whatsapp";
 
 export default function Home() {
-  console.log(process.env.RESEND_API_KEY, "RESEND_API_KEY");
   return (
     <>
       <Principal />
