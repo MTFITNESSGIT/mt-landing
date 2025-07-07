2de447d6ae3220aeb7b3f3bf2162bbd3c6acac17
Juan Segundo <juansepincha@gmail.com>
2025-07-07 12:31:33 -0300

    feat: :art: update player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -12,8 +12,8 @@ const Player: React.FC<VideoPlayerProps> = ({ src }) => {
     <div className="w-full min-w-[320px] max-w-[1030px]">
       {!isLoaded && (
         <div
-          className="w-full rounded-[30px] min-w-[2
-        00px] sm:min-w-[500px] md:min-w-[700px] lg:min-w-[900px] xl:min-w-[1000px] max-w-[1030px] h-[530px] bg-neutral-800 animate-pulse border border-neutral-700 shadow-md"
+          className="w-full md:rounded-[30px] min-w-[2
+        00px] h-[235px] sm:min-w-[500px] md:min-w-[700px] lg:min-w-[900px] xl:min-w-[1000px] max-w-[1030px] md:h-[530px] bg-neutral-800 animate-pulse border border-neutral-700 shadow-md"
         />
       )}
 
